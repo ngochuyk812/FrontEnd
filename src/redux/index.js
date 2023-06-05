8e455fbace4ee8fe07aefd885437a5a6521389f2
phuochau132 <113565950+phuochau132@users.noreply.github.com>
2023-06-05 07:34:11 +0700

    thêm chức năng hiển thị sản phẩm và thêm sản phẩm vào cart trang home

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,15 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 
 import authSlice from "./slice/authSlice";
-import notifySlice from './slice/notifySlice';
-import registerSlice from './slice/registerSlice';
+import notifySlice from "./slice/notifySlice";
+import registerSlice from "./slice/registerSlice";
 import productSlice from "./slice/productSlice";
-export const store  = configureStore({
+import cartSlide from "./slice/cartSlide";
+export const store = configureStore({
   reducer: {
     auth: authSlice,
     register: registerSlice,
     products: productSlice,
-    notify: notifySlice
-        }
-})
- 
+    notify: notifySlice,
+    cart: cartSlide,
+  },
+});
